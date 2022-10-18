71b7649f0263dcbeeabd1f88506779ebbbeafe1b
mechztcc <dojathan@gmail.com>
2022-10-18 14:58:11 -0300

    feat: added Form component

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <div class="column is-one-quarter">
       <SideNav />
     </div>
-    <div class="column isthree-quarter"></div>
+    <div class="column isthree-quarter">
+      <Form />
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import SideNav from './components/SideNav.vue';
+import Form from './components/Form.vue';
 
 
 export default defineComponent({
   name: "App",
-  components: { SideNav }
+  components: { SideNav, Form }
 });
 </script>
 
